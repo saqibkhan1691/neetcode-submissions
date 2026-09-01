454e72e184f7d78756bc9e5f6a600a2444ae8c32
Saqib Khan <saqibkhan4428960@gmail.com>
2026-09-02 01:39:43 +0530

    Enhance comments for clarity in profit calculation
    Added comments to clarify logic for maximum profit calculation.

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -8,10 +8,13 @@ class Solution {
 
         int maxProfit = 0;
 
+        // we dont take last because last day par buy karke future mein sell nahi kar sakte
         for (int index = 0; index < n - 1; index++) {
 
+            // Ab tumhe index 1 ke baad maximum selling price find karna hai
             int max = prices[index];
 
+            // index+1 means buy wale din ke baad se checking start hogi
             for (int i = index + 1; i < n; i++) {
                 if (prices[i] > max) {
                     max = prices[i];
